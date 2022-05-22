3d949ea30900bc7bdda8f76420815020f0c1592c
SolijonovDev <solijonovqodirjon@gmail.com>
2022-05-22 08:01:44 +0500

    added blog page

---
+++ pages/blog.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+
+const Blog=()=>{
+    return <>
+     <h1>Blog page</h1>
+     <Link href="/"><a>Home</a></Link>
+    </>
+}
+
+export default Blog;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
           <p>hello world</p>
           <p>hello nextjs</p>
           <p>lorem impus</p>
+          <Link href="/blog"><a>Blog</a></Link>
         </div>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
