1bada1fdcf53c3acfab58b03920ce2fd27d31ed7
SolijonovDev <solijonovqodirjon@gmail.com>
2022-05-22 09:07:26 +0500

    change Image

--- pages/users.jsx
+++ pages/users.jsx
@@ -1,48 +1,50 @@
 import Link from "next/link";
 import { Layout } from "./../components/Layout";
-import  Image  from 'next/image';
+import Image from "next/image";
+
+const myLoader = ({ src, width, quality }) => {
+    return `${src}?w=${width}&q=${quality || 75}`
+  }
 
 const Users = (props) => {
   return (
     <Layout title={"users"}>
       <h1>Users</h1>
       <div>
-          {props.movies.movies.map(v=>(
-              <div key={v.id}>
-                  <Image
-                      loader={() => v.files.poster_url}
-                      src={v.files.poster_url}
-                      width={500}
-                      height={200}
-                    //   layout="fill"
-                      alt="photo"
-                    />
-                <Link href={`/user/${v.id}`}>
-                
-                   <a><span>{v.id}</span> <span>{v.title}</span>  <span>{v.year}</span></a>
-                </Link>
-              </div>
-          ))}
+        {props.movies.movies.map((v) => (
+          <div key={v.id}>
+            <Image
+              loader={myLoader}
+              src={v.files.poster_url}
+              width={500}
+              height={200}
+              //   layout="fill"
+              alt="photo"
+            />
+            <Link href={`/user/${v.id}`}>
+              <a>
+                <span>{v.id}</span> <span>{v.title}</span> <span>{v.year}</span>
+              </a>
+            </Link>
+          </div>
+        ))}
       </div>
     </Layout>
   );
 };
 
-
-
 export async function getServerSideProps(context) {
-    console.log("users context: ",context)
-    const res = await fetch(
-        `https://api.itv.uz/api/content/main/2/list?page=${1}&user=a71651276800s055120e1a6ffaa1ec27`
-      );
-      const json = await res.json();
-  
-    return {
-      props: {
-        movies:json.data,
-      },
-    }
-  }
+  console.log("users context: ", context);
+  const res = await fetch(
+    `https://api.itv.uz/api/content/main/2/list?page=${1}&user=a71651276800s055120e1a6ffaa1ec27`
+  );
+  const json = await res.json();
 
+  return {
+    props: {
+      movies: json.data,
+    },
+  };
+}
 
 export default Users;
